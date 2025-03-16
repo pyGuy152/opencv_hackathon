455780b7243263013af411fb763111a950cb8bbc
Sarvesh sabe <Sarveshsabe@gmail.com>
2025-03-16 00:04:23 -0500

    init

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/api', methods=['POST'])
+def api():
+    try:
+        data = request.get_json()  # Attempt to parse JSON data
+        if data is None:
+            return jsonify({"error": "Invalid JSON data"}), 400 #Return an error if not valid JSON
+        return jsonify({"message": "API POST request received", "data": data})
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400 #Return an error if the request is not valid.
+
+if __name__ == '__main__':
+    app.run(debug=True)
